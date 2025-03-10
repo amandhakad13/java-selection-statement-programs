be308bd0477081b9ba9371e47d1bc2c6aa749e1a
= <=>
2025-03-09 21:30:50 -0700

    questions

---
+++ if-statement/Even.java
@@ -0,0 +1,8 @@
+public class Even {
+	public static void main(String[] args) {
+		int age = 20;
+		
+		if(age >= 18)
+			System.out.println("You are eligible to vote");
+	}
+}
---
+++ if-statement/Vowel.java
@@ -0,0 +1,8 @@
+public class Vowel {
+	public static void main(String[] args) {
+		char ch = 'e';
+		
+		if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u')
+			System.out.println(ch+" is a Vowel");
+	}
+}
